12ba1d100e6906388bff6071ca271bc80f6258c3
0xThreeBody <34789555+jiangfuyao@users.noreply.github.com>
2022-03-21 12:03:36 +0800

    fix gasLimit

--- src/utils/mandala.ts
+++ src/utils/mandala.ts
@@ -4,16 +4,19 @@ export async function getMandalaGasParams(rpc: string): Promise<{
   gasPrice: number;
   gasLimit: number;
 }> {
-  const gasLimit = 800000;
-  const storageLimit = 1500;
   const res = (
     await axios.post(rpc, {
       id: 0,
       jsonrpc: "2.0",
       method: "eth_getEthGas",
-      params: [gasLimit, storageLimit],
+      params: [{
+          "gasLimit": 800000,
+          "storageLimit": 1500
+      }]
     })
   ).data.result;
+
+  console.log(res)
 
   return {
     gasLimit: parseInt(res.gasLimit, 16),
